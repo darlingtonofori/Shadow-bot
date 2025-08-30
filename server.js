5e83a77a126e2190395f9a9e7239e24223eca386
TR4N5P4R3NT <159647064+darlingtonofori@users.noreply.github.com>
2025-08-30 14:01:44 +0000

    Create server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Serve static files
+app.use(express.static(__dirname));
+
+// All routes serve the index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`NGX5 Pairing service running on port ${port}`);
+});
